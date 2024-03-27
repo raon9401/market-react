f9d4f581398621b38efecc68530b07da38c4c8e9
raon9401 <raon9401@naver.com>
2024-03-27 23:34:56 +0900

    Add: style change, useEffect add

---
+++ src/component/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// 1. 상품 이미지
+// 2. Conscious choice
+// 3. 상품 이름
+// 4. 가격
+// 5. 신제품 or 기존제품
+
+const ProductCard = () => {
+    return (
+        <div>ProductCard</div>
+    )
+}
+
+export default ProductCard
--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProducts } from '../api/products';
 
 const ProductAll = () => {
+    const [productList, setProductList] = useState([]);
+
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
     return (
         <div></div>
     )
